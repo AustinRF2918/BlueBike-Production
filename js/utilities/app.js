2c54e1633e61f2573e4336ef30a91d3e446eb9d7
Austin Fell <austinrf2918@yahoo.com>
2016-06-08 19:11:02 -0400

    Fixed bugs

--- js/utilities/app.js
+++ js/utilities/app.js
@@ -6,23 +6,54 @@ $(".hover-icon").hide();
 $("#navbar-mobile-toggler").mousedown(function(){
     if (navStatus == false)
     {
-	offset = $(this).offset();
-	height = 40;
-
-	$(".bb-navbar-toggle").addClass("bb-navbar-enabled", transitTime, "easeInElastic");
-	$(".bb-navbar-toggle").addClass("bb-navbar-toggle-active", transitTime, "easeInElastic");
-	$(".bb-navbar-toggle").removeClass("bb-navbar-disabled", transitTime, "easeOutElastic");
-	$(".bb-navbar-toggle").removeClass("bb-navbar-toggle-inactive", transitTime, "easeOutElastic");
-	navStatus = true;
+	enableMobileNavigation();
+	
     }
     else
     {
-	$(".bb-navbar-toggle").addClass("bb-navbar-disabled", transitTime, "easeInElastic");
-	$(".bb-navbar-toggle").addClass("bb-navbar-toggle-inactive", transitTime, "easeInElastic");
-	$(".bb-navbar-toggle").removeClass("bb-navbar-enabled", transitTime, "easeInElastic");
-	$(".bb-navbar-toggle").removeClass("bb-navbar-toggle-active", transitTime, "easeInElastic");
-	navStatus = false;
+	disableMobileNavigation();
     }
 	
 });
+
+//Make call by references to make reusable.
+var enableMobileNavigation = function()
+{
+    offset = $(this).offset();
+    height = 40;
+
+    $(".bb-navbar-toggle").addClass("bb-navbar-enabled", transitTime, "easeInElastic");
+    $(".bb-navbar-toggle").addClass("bb-navbar-toggle-active", transitTime, "easeInElastic");
+    $(".bb-navbar-toggle").removeClass("bb-navbar-disabled", transitTime, "easeOutElastic");
+    $(".bb-navbar-toggle").removeClass("bb-navbar-toggle-inactive", transitTime, "easeOutElastic");
+    navStatus = true;
+};
+
+//Make call by references to make reusable.
+var disableMobileNavigation = function()
+{
+    $(".bb-navbar-toggle").addClass("bb-navbar-disabled", transitTime, "easeInElastic");
+    $(".bb-navbar-toggle").addClass("bb-navbar-toggle-inactive", transitTime, "easeInElastic");
+    $(".bb-navbar-toggle").removeClass("bb-navbar-enabled", transitTime, "easeInElastic");
+    $(".bb-navbar-toggle").removeClass("bb-navbar-toggle-active", transitTime, "easeInElastic");
+    navStatus = false;
+};
+
+var disableConditionalMobileNavigation = function()
+{
+    if (navStatus == true)
+    {
+	disableMobileNavigation();
+    }
+};
+
+$(window).resize(function(){
+    disableConditionalMobileNavigation();
+});
+
+$(window).scroll(function(){
+    disableConditionalMobileNavigation();
+});
+
+
 
